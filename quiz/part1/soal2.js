7785995a1d2693197f2b87402ed6e823830d6763
zallnugraha11 <zallnugraha@gmail.com>
2025-03-10 07:03:47 +0700

    submit Tugas ke 1 week 2

---
+++ quiz/part1/soal2.js
@@ -0,0 +1,42 @@
+//pada soal yg kedua, kalian harus belajar method `splice`, `slice`, `join`, 'split', dan lainnya
+
+let input = ["0001", "Roman Alamsyah ", "Bandar Lampung", "21/05/1989", "Membaca"];
+let bulan = ""
+;
+function dataHandling(input) {
+
+    input.splice(1, 1, "Roman Alamsyah Elsharawy"), 
+    input.splice(2, 1, "Provinsi" + input[2]),
+    input.splice(4, 1, "Pria", "SMA International Metro");
+    console.log(input)
+
+    let namabulan= ""
+        switch (bulan) {
+            case 1: namabulan = "Mei";
+                
+                break;
+            default: namabulan = "Mei";
+        }
+        console.log(namabulan)
+
+    let tanggalArray = input[3].split("/");
+    let formatdate = [tanggalArray[2], tanggalArray[0], tanggalArray[1]]
+    let formatdate1 = `${tanggalArray[0]}-${tanggalArray[1]}-${tanggalArray[2]}`
+    let limitedName = input[1].substring(0, 15);
+    
+    console.log(formatdate)
+    console.log(formatdate1)
+    console.log(limitedName)
+
+}
+dataHandling(input);
+
+/**
+ * keluaran yang diharapkan (pada console)
+ *
+ * ["0001", "Roman Alamsyah Elsharawy", "Provinsi Bandar Lampung", "21/05/1989", "Pria", "SMA Internasional Metro"]
+ * Mei
+ * ["1989", "21", "05"]
+ * 21-05-1989
+ * Roman Alamsyah //batasi hanya 15 karakter saja pada array elemen ke 2
+ */
